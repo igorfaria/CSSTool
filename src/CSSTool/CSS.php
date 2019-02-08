5df3902f6633e05df85be5266ed1fd19e29bc3fe
IgorFaria <igorthiagofaria@gmail.com>
2019-02-08 18:58:35 +0000

    :D :D

--- src/CSSTool/CSS.php
+++ src/CSSTool/CSS.php
@@ -4,5 +4,46 @@ namespace CSSTool;
 
 class CSS
 {
+    private $parsed_css = [];
+
+    public function get(){
+        return $this->parsed_css;
+    }
+    public function set($css_input){
+        $this->parsed_css = [];
+        $this->add($css_input);
+    }
+
+    public function parse($string_css){
+        if(is_null($string_css)) return [];
+        $Parser = new Tools\Parser();
+        return $Parser->parse($string_css);
+    }
+
+    public function append($css_input){
+        return $this->add($css_input, true);
+    }
+
+    public function prepend($css_input){
+        return $this->add($css_input, false);
+    }
+
+    private function add($css_string_or_array,$append=true){
+        if(!is_string($css_string_or_array) AND !is_array($css_string_or_array)) return false;
+
+        $css_input = $css_string_or_array;
+
+        if(is_string($css_string_or_array)){
+            $css_input = $this->parse($css_string_or_array);
+        }
+
+        if($append){
+            $this->parsed_css = array_merge($this->parsed_css, $css_input);
+        } else{
+            $this->parsed_css = array_merge($css_input,$this->parsed_css);
+        }
+
+        return true;
+    }
     
 }
--- src/CSSTool/Tools/Parser.php
+++ src/CSSTool/Tools/Parser.php
@@ -4,5 +4,112 @@ namespace CSSTool\Tools;
 
 class Parser
 {
+    private $CSS = [];
+    private $config = [];
+
+    public function __construct(){
+        return $this;
+    }
+
+    public function parse($css_text){
+        // If it isn't an string...
+        if(!is_string($css_text)) return false;
+        // Remove comments before parsing
+        $css_text = preg_replace('@\\s*/\\*.*\\*/\\s*@sU','', $css_text);
+        // Remove html comments
+        $css_text = preg_replace('/([^\'"]+?)(\<!--|--\>)([^\'"]+?)/ms', '$1$3', $css_text);
+        /* Remove empty selectors
+        $css_text = preg_replace('/(?<=(\}|;))[^\{\};]+\{\s*\}/', '', $css_text)*/
     
+        $re_media = "/(@\w+[^{]+)\{([\s\S]+?})\s*}/";
+        preg_match_all($re_media, $css_text, $matches_media);
+
+        $css_text_aux = $this->replaceMedias($re_media, $css_text);
+        
+        # Initial Source: https://stackoverflow.com/questions/33547792/php-css-from-string-to-array
+        //$re_css = "/\s*([^{]+)\s*\{\s*([^}]*?)\s*}/";
+        $re_css = "/([^{]+)\s*\{\s*([^}]+)\s*}/";
+        preg_match_all($re_css, $css_text_aux, $matches);
+        
+        //Create an array to hold the returned values
+        $return = array();
+        for($i = 0; $i<count($matches[0]); $i++){
+            //Get the ID/class
+            $name = trim($matches[1][$i]);
+            //Get the rules
+            $rules = trim($matches[2][$i]);
+            //Format rules into array
+            $rules_a = array();
+            $rules_x = explode(";", $rules);
+            foreach($rules_x as $r){
+                if(trim($r)!=""){
+                    $s = explode(":", $r);
+                    if(empty($s[1])) continue;
+                    $property = trim($s[0]);
+                    $value = trim($s[1]);
+                    
+                    // Autoprefixer :D
+                    if(isset($this->config['autoprefixer']) AND $this->config['autoprefixer']){
+                        // Check if is there is an array with prefixes
+                        if(is_array($this->config['prefixes'])){
+                            // Iterates over 
+                            foreach($this->config['prefixes'] as $prefix=>$pre_prop){
+                                // If the property is defined in the array
+                                if(in_array($property, $pre_prop)){
+                                    // Add property with prefix to the rules
+                                    $rules_a[$prefix . $property] = $value;
+                                }
+                            }
+                        }
+                    }
+                    // Add property to the rules
+                    $rules_a[$property] = $value;
+                }
+            }
+            // Media queries
+            if(preg_match('/\@media-(\d+)/',$name, $media_query)) {
+                // Check if there is the number that identify the media
+                if(isset($media_query[1]) AND isset($matches_media[0][$media_query[1]])){
+                    // @media print, @keyframes, @media all and (max-width: ...)
+                    $media_query_name = $matches_media[1][$media_query[1]];
+                    $name = trim($media_query_name);
+                    // CSS string related to the media
+                    $media_query_css = $matches_media[2][$media_query[1]];
+                    // Parse the CSS string
+                    $rules_a = $this->parse($media_query_css); 
+                    // If autoprefixer is true, there is the -webkit- prefix and is @keyframes
+                    if(isset($this->config['autoprefixer']) 
+                    AND $this->config['autoprefixer'] 
+                    AND isset($this->config['autoprefixer']['prefixes']['-webkit-']) 
+                    AND strpos($name,'@keyframes') !== false){
+                        // Add the webkit prefix to @keyframes
+                        $name_webkit = preg_replace('/@keyframes/i','@-webkit-keyframes', $name);
+                        // Append to the array
+                        $return[][$name_webkit] = $rules_a;
+                        
+                    }
+                } 
+            }
+            
+            
+            //Add the name and its values to the array
+            $return[][$name] = $rules_a;
+        }
+        //Return the array with rules
+        return $return;
+    }
+
+    /*
+        Int to count the @medias and @keyframes
+    */
+    private $_countMedias = 0;
+    
+    private function replaceMedias($pattern, $text) {
+        $this->_countMedias = 0;
+        return preg_replace_callback($pattern, array($this, '_callbackMedias'), $text);
+    }
+    public function _callbackMedias($matches) {
+        return '@media-' . $this->_countMedias++ . '{media:'.$this->_countMedias.'}';
+    }
+
 }
