0658c95c52e61b392c3e8723656bc589809c6205
IgorFaria <itfaria@gmail.com>
2019-02-24 00:02:35 +0000

    Adding a big CSS file to the tests

---
+++ tests/load_BigCSS.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Testing to load a simple CSS from a local file
+*/
+header('Content-type: text/plain;charset=utf8');
+
+// Include the autoload :D
+require  'vendor/autoload.php';
+
+// Create an instance 
+$CSS = new CSSTool\CSS;
+echo 'Load local file "./css/big.css"' . PHP_EOL . PHP_EOL;
+$CSS->load('css/big.css');
+// Output
+var_dump($CSS->get());
